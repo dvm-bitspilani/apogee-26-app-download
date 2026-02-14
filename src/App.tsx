ce3112bf8ac30907ebea0ebd3f5fa28ebee9b1ba
Rishit Verma <63300310+rish-kun@users.noreply.github.com>
2026-02-14 21:49:44 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -73,7 +73,8 @@ function App() {
           </a>
 
           <a
-            href="https://sgp.cloud.appwrite.io/v1/storage/buckets/6989f36b003e78eeb2af/files/699031a6003c0547933e/download?project=690ba549001e1fcf8e38&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbklkIjoiNjk5MDMyNGY5NTdlZjBjYmQ4YTAiLCJyZXNvdXJjZUlkIjoiNjk4OWYzNmIwMDNlNzhlZWIyYWY6Njk5MDMxYTYwMDNjMDU0NzkzM2UiLCJyZXNvdXJjZVR5cGUiOiJmaWxlcyIsInJlc291cmNlSW50ZXJuYWxJZCI6IjI5MjE6NCIsImlhdCI6MTc3MTA1Nzc0MywiZXhwIjoxNzcxNjMyMDAwfQ.VS4frj6As4Q_KJiLPBnnPsfXFb2WiNnAQL_Y0i34kBs"
+            href="https://drive.google.com/drive/folders/1coIfhElK5TWtaDc_99cO7vOKniMUGfVT"
+            //href="https://sgp.cloud.appwrite.io/v1/storage/buckets/6989f36b003e78eeb2af/files/699031a6003c0547933e/download?project=690ba549001e1fcf8e38&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbklkIjoiNjk5MDMyNGY5NTdlZjBjYmQ4YTAiLCJyZXNvdXJjZUlkIjoiNjk4OWYzNmIwMDNlNzhlZWIyYWY6Njk5MDMxYTYwMDNjMDU0NzkzM2UiLCJyZXNvdXJjZVR5cGUiOiJmaWxlcyIsInJlc291cmNlSW50ZXJuYWxJZCI6IjI5MjE6NCIsImlhdCI6MTc3MTA1Nzc0MywiZXhwIjoxNzcxNjMyMDAwfQ.VS4frj6As4Q_KJiLPBnnPsfXFb2WiNnAQL_Y0i34kBs"
             // href="https://sgp.cloud.appwrite.io/v1/storage/buckets/6989f36b003e78eeb2af/files/698f45a700224b928619/download?project=690ba549001e1fcf8e38&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbklkIjoiNjk4ZjQ2MjI4Yjk5M2I0NmZlMTYiLCJyZXNvdXJjZUlkIjoiNjk4OWYzNmIwMDNlNzhlZWIyYWY6Njk4ZjQ1YTcwMDIyNGI5Mjg2MTkiLCJyZXNvdXJjZVR5cGUiOiJmaWxlcyIsInJlc291cmNlSW50ZXJuYWxJZCI6IjI5MjE6MSIsImlhdCI6MTc3MDk5NzI4MiwiZXhwIjoxNzcxNTQ1NjAwfQ.0--gIRspQPh6qnIaBHrozuZW6PSJhYwOYRdwzJJk6QI"
             className="cyber-btn secondary"
             // onClick={(e) => e.preventDefault()}
